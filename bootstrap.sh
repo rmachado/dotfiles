f327af0a073a28ca0a94eecf2561985bdfd44055
Rodrigo <rml22388@gmail.com>
2025-05-23 21:30:07 -0400

    initial config files

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# =============================================================
+# Install dependencies
+# =============================================================
+
+# Oh My Zsh (https://github.com/ohmyzsh/ohmyzsh)
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Oh My Posh (https://ohmyposh.dev/docs/installation)
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+
+# =============================================================
+# Set up dot files
+# =============================================================
+
+# Zsh
+ln -s $PWD/.config/zshrc $HOME/.config
+ln -s $PWD/.zshrc $HOME/.zshrc
+
+# Tmux
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+
+# NeoVim
+ln -s $PWD/nvim $HOME/.config/nvim
