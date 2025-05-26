31b42f0a85e3ba272e3b9aa450eb09fd182cba45
Rodrigo <rml22388@gmail.com>
2025-05-26 09:55:45 -0400

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,16 +10,23 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 # Oh My Posh (https://ohmyposh.dev/docs/installation)
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 
+# Fonts
+sudo pacman -S ttf-jetbrains-mono-nerd
+
 # =============================================================
 # Set up dot files
 # =============================================================
 
 # Zsh
 ln -s $PWD/.config/zshrc $HOME/.config
+ln -s $PWD/.config/ohmyposh $HOME/.config
 ln -s $PWD/.zshrc $HOME/.zshrc
 
 # Tmux
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 
+# Kitty
+ln -s $PWD/.config/kitty $HOME/.config
+
 # NeoVim
-ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/nvim $HOME/.config
