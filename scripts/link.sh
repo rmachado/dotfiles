1e6d67175d794b8c90a2a240c29f8c219cbac680
Rodrigo Machado <rml22388@gmail.com>
2025-06-15 11:55:06 -0400

    update config

--- scripts/link.sh
+++ scripts/link.sh
@@ -8,10 +8,12 @@ HOME_LINKS=(.zshrc .tmux.conf)
 CONFIG_LINKS=(hypr kitty ohmyposh waybar wpaperd zshrc)
 
 for file in "${HOME_LINKS[@]}"
+do
 	ln -s "$PWD/$file" "$HOME/$file"
-end
+done
 
 for file in "${CONFIG_LINKS[@]}"
-	ln -s "$PWD/.config/$file" "$HOME/.config"
-end
+do
+	ln -s "$PWD/config/$file" "$HOME/.config/$file"
+done
 
